457fd501924a36fb62d633c3286f4967fdbc11fc
Batın Evirgen <batinevirgen@gmail.com>
2023-02-17 21:43:25 +0300

    fix: change svg props to react compatible names (camelCase)

--- src/components/ui/Icons.tsx
+++ src/components/ui/Icons.tsx
@@ -49,8 +49,8 @@ export function ChevronDownIcon({ ...props }: BaseIconProps) {
   return (
     <BaseIcon {...props}>
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M19.5 8.25l-7.5 7.5-7.5-7.5"
       />
     </BaseIcon>
