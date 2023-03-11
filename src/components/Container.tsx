0bbe20f58d6ff384dfc6fa29e832b8377c5645ee
Batın Evirgen <batinevirgen@gmail.com>
2023-03-11 20:16:39 +0300

    chore: add 4px padding to container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -3,7 +3,7 @@ import { cn } from '@/utils/tw';
 interface ContainerProps extends React.ComponentPropsWithoutRef<'div'> {}
 export default function Container({ children, className, ...props }: ContainerProps) {
   return (
-    <div className={cn('mx-auto max-w-6xl', className)} {...props}>
+    <div className={cn('mx-auto max-w-6xl px-1', className)} {...props}>
       {children}
     </div>
   );
