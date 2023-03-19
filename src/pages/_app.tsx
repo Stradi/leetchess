714e36413bdfd55e4e9d95b049e7cb6164da07a8
Batın Evirgen <batinevirgen@gmail.com>
2023-03-19 18:30:19 +0300

    feat: add Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,30 @@ import Container from '@/components/Container';
 import Breadcrumb from '@/components/ui/Breadcrumb';
 import NavigationBar from '@/components/ui/NavigationBar';
 import config from '@/config/config';
-import type { AppProps } from 'next/app';
+import { GoogleAnalytics, event } from 'nextjs-google-analytics';
+import type { AppProps, NextWebVitalsMetric } from 'next/app';
 import '../globals.css';
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  event(metric.name, {
+    category: metric.label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+    value: Math.round(metric.name === 'CLS' ? metric.value * 1000 : metric.value),
+    label: metric.id,
+    nonInteraction: true,
+  });
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Container>
-      <NavigationBar items={config.navigation} />
-      <br></br>
-      <Breadcrumb whitelistedQueryParams={['filter']} />
-      <br></br>
-      <Component {...pageProps} />
-    </Container>
+    <>
+      <GoogleAnalytics trackPageViews />
+      <Container>
+        <NavigationBar items={config.navigation} />
+        <br></br>
+        <Breadcrumb whitelistedQueryParams={['filter']} />
+        <br></br>
+        <Component {...pageProps} />
+      </Container>
+    </>
   );
 }
